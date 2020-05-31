a6443bfbb71c9002cb23cd62f81fa3d751398f15
Kouhei Sano <apple.x.co@gmail.com>
2020-05-31 14:02:51 +0900

    feat: ddd

--- source/app/src/Resource/Page/Index.php
+++ source/app/src/Resource/Page/Index.php
@@ -1,10 +1,24 @@
 <?php
 namespace MyVendor\MyProject\Resource\Page;
 
+use AppCore\Application\User\UserApplicationService;
 use BEAR\Resource\ResourceObject;
 
 class Index extends ResourceObject
 {
+    /** @var UserApplicationService */
+    private $userApplicationService;
+
+    /**
+     * Index constructor.
+     *
+     * @param UserApplicationService $userApplicationService
+     */
+    public function __construct(UserApplicationService $userApplicationService)
+    {
+        $this->userApplicationService = $userApplicationService;
+    }
+
     public function onGet(string $name = 'BEAR.Sunday') : ResourceObject
     {
         $this->body = [
