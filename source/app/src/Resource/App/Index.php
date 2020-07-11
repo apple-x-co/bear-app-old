eda8e26ba00e44006603f8eebffb0b69be808123
Kouhei Sano <apple.x.co@gmail.com>
2020-07-11 16:35:45 +0900

    chore: update CS

--- source/app/src/Resource/App/Index.php
+++ source/app/src/Resource/App/Index.php
@@ -8,29 +8,6 @@ use BEAR\Resource\ResourceObject;
 
 class Index extends ResourceObject
 {
-    /** @var array{overview: string, issue: string, _links: string} */
-    public $body = [
-        'overview' => 'This is the BEAR.App REST API',
-        'issue' => 'https://github.com/apple-x-co/BEAR.App/issues',
-        '_links' => [
-            'self' => ['href' => '/'],
-            'curies' => [
-                'href' => 'rels/{rel}.html',
-                'name' => 'app',
-                'templated' => true,
-            ],
-            'app:user' => [
-                'href' => '/users/{id}',
-                'title' => 'The user item',
-                'templated' => true,
-            ],
-            'app:users' => [
-                'href' => '/users',
-                'title' => 'The user list',
-            ],
-        ],
-    ];
-
     public function onGet(): ResourceObject
     {
         return $this;
